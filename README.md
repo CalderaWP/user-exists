ba584b0b20ce6664d5cba918861b657d4eeb97fb
Josh Pollock <Josh@JoshPress.net>
2017-04-06 14:32:06 -0400

    use login lockdown to lock out repeated fails

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 This is a WordPress plugin to check if a user exists on the site by email.
 
 GET `wp-json/calderawp/user-exists?email=roy%40roysivan.com` returns true if Roy is a user on your site, false if not. Also, Hi Roy.
+
+# Read This RE: Security/ Privacy
+This plugin, when active could make brute forcing your login easier, since it could be used to check for user before trying a stolen password for that user. It could also leak your user list. I could say the same thing about the WordPress registration form, but you would probably tell me you have a brute force protection plugin and/ or a WAF to stop that.
+
+This plugin integrates with [Login Lockdown](https://wordpress.org/plugins/login-lockdown/). If Login Lockdown is active, failed user name exists checks will be registered as a failed login *if you have the "Lockout Invalid Usernames?" setting checked.* If a username check fails more than the amount of times you allowed in Login LockDown settings, then a lock out is triggered and all user exists checks are blocked until the lockout is lifted according to Login Lockdown settings.
+
+If you wish to create your own system, for throttling or brute force protection The "cwp_user_exits_failed" action and "cwp_user_exists_pre_check" filter are provided. If you return a WP_Error at "cwp_user_exists_pre_check" filter, then the check will not run and the REST API will return an error. Use this to count past fails or check a single use token or a nonce. When a user exists check fails the "cwp_user_exits_failed"  action is fired. Use this to count fails.
--- user-exists.php
+++ user-exists.php
@@ -9,25 +9,102 @@ add_action( 'rest_api_init', function(){
 		'args' => [
 			'email' => [
 				'type' => 'string',
-				'required' => true
+				'required' => true,
 			]
 		],
 		'callback' => function( $request ){
-			$pre_serve = apply_filters( 'user_exists_pre_check', $request );
+			/**
+			 * Hook to prevent check
+			 *
+			 * Use to impliment throttling
+			 *
+			 * @param null|WP_Error Return a WP_Error to stop check and return that error via API
+			 * @param string $email Email address
+			 * @param WP_REST_Request $request  Current request
+			 */
+			$pre_serve = apply_filters( 'cwp_user_exists_pre_check', null,  $request[ 'email' ], $request );
 			if( is_wp_error( $pre_serve ) ){
 				return rest_ensure_response( $pre_serve );
 			}
+
 			if( is_email( $request[ 'email' ] ) && is_numeric( email_exists( $request[ 'email' ] ) ) ){
 				return rest_ensure_response( [ 'exists' => true ] );
 			}
+
 			/**
 			 * Fires when user exist check fails
 			 *
-			 * @param $request WP_REST_Request Current request
+			 * @param string$email Email address
+			 * @param WP_REST_Request $request  Current request
 			 */
-			do_action( 'user_exits_failed', $request );
+			do_action( 'cwp_user_exits_failed', $request[ 'email' ], $request );
 			return rest_ensure_response( [ 'exists' => false ] );
 
 		}
 	]);
+});
+
+/**
+ * Trigger login lock down blocks if login lockdown exists
+ */
+add_action( 'plugins_loaded', function(){
+	if( function_exists( 'loginLockdown_install' ) ){
+
+		/**
+		 * Increment and count fails
+		 */
+		add_action( 'cwp_user_exits_failed', function( $email  ){
+			incrementFails( $email );
+			$fails = countFails( $email );
+			$settings = get_loginlockdownOptions();
+			$allowed = 3;
+			if( ! empty( $settings[ 'max_login_retries' ] ) ){
+				$allowed = $settings[ 'max_login_retries' ];
+			}
+
+
+			//To many fails, you are now locked out.
+			if( cwp_user_exist_is_to_many( $email ) ){
+				lockDown( $email );
+			}
+		});
+
+		/**
+		 * Prevent user exists check if locked out
+		 */
+		add_filter( 'cwp_user_exists_pre_check', function( $return, $email ){
+			//If locked out, return WP_Error
+			if( isLockedDown() ||cwp_user_exist_is_to_many( $email ) ){
+				return new WP_Error( 'user-exists-lockout', __( 'Locked out' ) );
+			}
+
+			return null;
+
+
+		}, 10, 2 );
+	}
+
+	/**
+	 * Check if too many lockouts
+	 *
+	 * @param string $email Email to check
+	 *
+	 * @return bool
+	 */
+	function cwp_user_exist_is_to_many( $email ){
+		$fails = countFails( $email );
+		$settings = get_loginlockdownOptions();
+		$allowed = 3;
+		if( ! empty( $settings[ 'max_login_retries' ] ) ){
+			$allowed = $settings[ 'max_login_retries' ];
+		}
+
+
+		//To many fails, you are now locked out.
+		if( $fails > $allowed ){
+			return true;
+		}
+
+		return false;
+	}
 });
